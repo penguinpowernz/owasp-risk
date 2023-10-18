ddd4625c24793602e5e62971821d1acef437b3dd
Robert McLeod <robert@penguinpower.co.nz>
2023-10-18 20:46:44 +1300

    add ability to customize likelihood and impact factors

---
+++ app/controllers/customize_controller.rb
@@ -0,0 +1,4 @@
+class CustomizeController < ApplicationController
+  def index
+  end
+end
---
+++ test/models/non_compliance_impact_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class NonComplianceImpactTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
